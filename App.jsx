1242a5b467f163824db2a974dbd06480435b47a3
Kunal Kamat <kunalkamat3@gmail.com>
2024-03-09 00:44:30 +0530

    icon and splash screen added

--- App.jsx
+++ App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 import store from './store/index';
 import RootContainer from './pages/rootContainer';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <RootContainer />
